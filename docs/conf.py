a60986cbf82c0c3ca87af0849cc528935d0746e5
TPO <tpo.pomgom@gmail.com>
2020-10-22 15:06:58 +0200

    Updated conf.py to work with readthedocs, use readthedocs's theme

--- docs/conf.py
+++ docs/conf.py
@@ -1,9 +1,20 @@
+""" sphinx configuration """
+
+import os
+import sys
+
+
+# Required for autodoc to find the modules to document, since they are not in
+# the docs directory.
+sys.path.insert(0, os.path.abspath('.'))
+
 extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.napoleon',
     'sphinx.ext.viewcode',
 ]
-html_theme = 'nature'
+html_theme = 'sphinx_rtd_theme'
+# html_theme = 'nature'
 # html_theme = 'classic'
 # html_theme_options = {
 #     'stickysidebar': True  # Only works with html_theme = 'classic'
@@ -30,7 +41,7 @@ master_doc = "index"
 # This values are all used in the generated documentation.
 # Usually, the release and version are the same,
 # but sometimes we want to have the release have an "rc" tag.
-project = "SublimeEnv"
-copyright = "2020, TPO"
+project = "business_plan"
+copyright = "2020 by TPO (tpo.pomgom@gmail.com)"
 author = "TPO (tpo.pomgom@gmail.com)"
 version = release = "2020.0.1"
